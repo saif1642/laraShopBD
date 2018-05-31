c64cbfe624974308a63917bb2563389a105ff9b7
saifdev1642 <saifdev1642@gmail.com>
2018-06-01 05:16:07 +0600

    set admin header , footer & Dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,11 +10,16 @@ class AdminController extends Controller
         if($request->isMethod('post')){
             $data = $request->input();
             if(Auth::attempt(['email' => $data['email'],'password'=> $data['password'],'admin'=>'1'])){
-                echo 'Success';die;
+                return redirect('/admin/dashboard');
             }else{
                 echo 'Failed';die;
             }
         }
         return view('admin.admin_login');
+    }
+
+    public function dashboard(){
+       
+        return view('admin.dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 
 Route::get('/admin','AdminController@login');
 Route::match(['get','post'],'/admin','AdminController@login');
+Route::get('/admin/dashboard','AdminController@dashboard');
 
 Auth::routes();
 
